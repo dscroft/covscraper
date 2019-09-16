c146f4bee172714a0e707cd1dccd767421c2fd7f
David <ac0745@coventry.ac.uk>
2019-09-16 08:52:37 +0100

    Added 2019-2020 calcs

--- covscraper/timetableapi.py
+++ covscraper/timetableapi.py
@@ -7,7 +7,8 @@ import urllib
 
 WEEKOFFSET = { "2016-2017": datetime.date(2016,7,17), \
                "2017-2018": datetime.date(2017,7,16), \
-               "2018-2019": datetime.date(2018,7,15) }
+               "2018-2019": datetime.date(2018,7,15), \
+               "2019-2020": datetime.date(2019,7,14) }
 
 def get_lecturer_timetable( session, date=datetime.datetime.now() ):
     """get the sessions timetabled for the person used to authenticate the current session"""
